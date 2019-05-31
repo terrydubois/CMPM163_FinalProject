07afbdee7702b64bc470f9c095a10dd5c9871094
Terry DuBois <36257165+terrydubois@users.noreply.github.com>
2019-05-31 16:58:49 -0700

    PSX snaps to rotation

--- CMPM163_FinalProject/Assets/Scripts/psxOpen.cs
+++ CMPM163_FinalProject/Assets/Scripts/psxOpen.cs
@@ -6,6 +6,9 @@ public class psxOpen : MonoBehaviour
 {
     public bool psxRot = true;
     public Vector3 psxRotAmount;
+    private float psxCurrentXRot = 0;
+    private float psxCurrentYRot = 0;
+    private float psxCurrentZRot = 0;
 
     public bool lidOpen = false;
     public float lidClosedXRot;
@@ -23,16 +26,29 @@ public class psxOpen : MonoBehaviour
     {
         // make PSX spin/float around
         if (psxRot) {
-
+            lidOpen = false;
+            transform.Rotate(psxRotAmount * Time.deltaTime);
+            psxCurrentXRot = transform.eulerAngles.x;
+            psxCurrentYRot = transform.eulerAngles.y;
+            psxCurrentZRot = transform.eulerAngles.z;
+        }
+        else {
+            psxCurrentXRot = Approach(psxCurrentXRot, 0, 8);
+            psxCurrentYRot = Approach(psxCurrentYRot, 0, 8);
+            psxCurrentZRot = Approach(psxCurrentZRot, 0, 8);
+            transform.rotation = Quaternion.Euler(psxCurrentXRot, psxCurrentYRot, psxCurrentZRot);
         }
 
 
-        // lidOpen or close lid of PSX
+        // open or close lid of PSX
         var currentXRotDest = (lidOpen) ? lidOpenXRot : lidClosedXRot;
         lidCurrentXRot = Approach(lidCurrentXRot, currentXRotDest, 12);
-        lid.transform.rotation = Quaternion.Euler(lidCurrentXRot, 0, 0);
+        lid.transform.rotation = Quaternion.Euler(lidCurrentXRot + transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z);
+        //lid.transform.Rotate(new Vector3(transform.rotation.x, transform.rotation.y, transform.rotation.z));
     }
 
+
+    // smoothly transition a value to a desired value
     float Approach(float value, float valueDest, float divisor)
     {
         if (value < valueDest) {
