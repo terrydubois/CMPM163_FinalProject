8eac553e590b8f8e5566deaad67ba61a62315c81
Terry DuBois <36257165+terrydubois@users.noreply.github.com>
2019-05-31 16:23:13 -0700

    Better variable names

--- CMPM163_FinalProject/Assets/Scripts/psxOpen.cs
+++ CMPM163_FinalProject/Assets/Scripts/psxOpen.cs
@@ -4,24 +4,33 @@ using UnityEngine;
 
 public class psxOpen : MonoBehaviour
 {
-    public bool open = false;
-    public float closedXRot;
-    public float openXRot;
-    private float currentXRot;
+    public bool psxRot = true;
+    public Vector3 psxRotAmount;
+
+    public bool lidOpen = false;
+    public float lidClosedXRot;
+    public float lidOpenXRot;
+    private float lidCurrentXRot;
 
     public GameObject lid;
 
     void Start()
     {
-        currentXRot = closedXRot;
+        lidCurrentXRot = lidClosedXRot;
     }
 
     void Update()
     {
-        var currentXRotDest = (open) ? openXRot : closedXRot;
-        currentXRot = Approach(currentXRot, currentXRotDest, 12);
+        // make PSX spin/float around
+        if (psxRot) {
 
-        lid.transform.rotation = Quaternion.Euler(currentXRot, 0, 0);
+        }
+
+
+        // lidOpen or close lid of PSX
+        var currentXRotDest = (lidOpen) ? lidOpenXRot : lidClosedXRot;
+        lidCurrentXRot = Approach(lidCurrentXRot, currentXRotDest, 12);
+        lid.transform.rotation = Quaternion.Euler(lidCurrentXRot, 0, 0);
     }
 
     float Approach(float value, float valueDest, float divisor)
