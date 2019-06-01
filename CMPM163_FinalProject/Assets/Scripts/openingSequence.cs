712c398131ab63761f5887c1ee17ba1785d3154d
Terry DuBois <36257165+terrydubois@users.noreply.github.com>
2019-05-31 18:22:45 -0700

    Opening sequence

---
+++ CMPM163_FinalProject/Assets/Scripts/openingSequence.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openingSequence : MonoBehaviour
+{
+    public GameObject cam;
+    public Light ceilingLight;
+    private float lightIntensityMin = 0;
+
+    public bool camSequenceOver = false;
+    public float yStart;
+    public float zStart;
+    public float yEnd;
+    public float zEnd;
+    public float camPanSpeed = 0.04f;
+    public float holdTimer;
+
+    void Start()
+    {
+        transform.position = new Vector3(0, yStart, zStart);
+        ceilingLight.intensity = 0;
+    }
+
+    void Update()
+    {
+        if (holdTimer <= 0) {
+            var newY = Approach(transform.position.y, yEnd, camPanSpeed);
+            var newZ = Approach(transform.position.z, zEnd, camPanSpeed);
+            transform.position = new Vector3(0, newY, newZ);
+        }
+        else {
+            holdTimer--;
+        }
+
+        // slowly bring in light
+        if (Mathf.Abs(transform.position.z - zEnd) < (Mathf.Abs(zStart - zEnd) / 2)) {
+            // flicker light
+            ceilingLight.intensity += Random.Range(-0.03f, 0.03f);
+            lightIntensityMin += 0.01f;
+            lightIntensityMin = Mathf.Min(lightIntensityMin, 0.75f);
+        }
+        ceilingLight.intensity = Mathf.Clamp(ceilingLight.intensity, lightIntensityMin, 1.5f);
+
+        // cam sequence is over if camera is close enough to its final destination
+        camSequenceOver = (Mathf.Abs(transform.position.y - yEnd) < 0.5f
+                        && Mathf.Abs(transform.position.z - zEnd) < 0.5f);
+
+        // fast-forward the cam sequence by pressing Q                        
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            holdTimer = 0;
+            camPanSpeed = 1;
+        }
+    }
+    
+    // transition a value to a desired value by linear speed
+    float Approach(float value, float valueDest, float speed)
+    {
+        if (Mathf.Abs(value - valueDest) < speed) {
+            return valueDest;
+        }
+        if (value < valueDest) {
+            return value += speed;
+        }
+        else if (value > valueDest) {
+            return value -= speed;
+        }
+        return value;
+    }
+}
--- CMPM163_FinalProject/Assets/Scripts/psxOpen.cs
+++ CMPM163_FinalProject/Assets/Scripts/psxOpen.cs
@@ -16,6 +16,7 @@ public class psxOpen : MonoBehaviour
     private float lidCurrentXRot;
 
     public GameObject lid;
+    public GameObject cam;
 
     void Start()
     {
@@ -24,8 +25,8 @@ public class psxOpen : MonoBehaviour
 
     void Update()
     {
-        // make PSX spin/float around
         if (psxRot) {
+            // rotate PSX around all floaty
             lidOpen = false;
             transform.Rotate(psxRotAmount * Time.deltaTime);
             psxCurrentXRot = transform.eulerAngles.x;
@@ -33,23 +34,36 @@ public class psxOpen : MonoBehaviour
             psxCurrentZRot = transform.eulerAngles.z;
         }
         else {
-            psxCurrentXRot = Approach(psxCurrentXRot, 0, 8);
-            psxCurrentYRot = Approach(psxCurrentYRot, 0, 8);
-            psxCurrentZRot = Approach(psxCurrentZRot, 0, 8);
+            // return to original rotation
+            psxCurrentXRot = ApproachSmooth(psxCurrentXRot, 0, 8);
+            psxCurrentYRot = ApproachSmooth(psxCurrentYRot, 0, 8);
+            psxCurrentZRot = ApproachSmooth(psxCurrentZRot, 0, 8);
             transform.rotation = Quaternion.Euler(psxCurrentXRot, psxCurrentYRot, psxCurrentZRot);
         }
 
-
         // open or close lid of PSX
         var currentXRotDest = (lidOpen) ? lidOpenXRot : lidClosedXRot;
-        lidCurrentXRot = Approach(lidCurrentXRot, currentXRotDest, 12);
+        lidCurrentXRot = ApproachSmooth(lidCurrentXRot, currentXRotDest, 12);
         lid.transform.rotation = Quaternion.Euler(lidCurrentXRot + transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z);
-        //lid.transform.Rotate(new Vector3(transform.rotation.x, transform.rotation.y, transform.rotation.z));
+
+
+        // if camera is pulled back and the player clicks, orient the console and open the lid
+        if (cam.GetComponent<openingSequence>().camSequenceOver) {
+            if (Input.GetMouseButtonDown(0)) {
+                Debug.Log("here");
+                if (psxRot) {
+                    psxRot = false;
+                }
+                else {
+                    lidOpen = !lidOpen;
+                }
+            }
+        }
     }
 
 
     // smoothly transition a value to a desired value
-    float Approach(float value, float valueDest, float divisor)
+    float ApproachSmooth(float value, float valueDest, float divisor)
     {
         if (value < valueDest) {
             value += Mathf.Abs(value - valueDest) / divisor;
@@ -57,7 +71,6 @@ public class psxOpen : MonoBehaviour
         else if (value > valueDest) {
             value -= Mathf.Abs(value - valueDest) / divisor;
         }
-
         return value;
     }
 }
